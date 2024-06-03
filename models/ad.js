bfcfee2dd3adf38c91e4f019fc7ea949fe0de1a7
Sławomir Toporek <toporek.slawomir@gmail.com>
2024-06-03 20:04:50 +0200

    add mongoose ad and user models

---
+++ models/ad.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const adSchema = new mongoose.Schema({
+  title: { type: String, required: true, minLength: 10, maxLength: 50 },
+  content: { type: String, required: true, minLength: 20, maxLength: 1000 },
+  publicationData: { type: Date, required: true },
+  image: { type: String, required: true },
+  price: { type: Number, required: true },
+  location: { type: String, required: true },
+  user: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' },
+});
+
+module.exports = mongoose.model('Ad', adSchema);
