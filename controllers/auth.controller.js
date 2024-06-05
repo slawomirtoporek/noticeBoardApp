12d74ac73fdedcbb4c36c0110311f7a69884af73
Sławomir Toporek <toporek.slawomir@gmail.com>
2024-06-05 19:56:37 +0200

    Add auth/login endpoint

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -20,13 +20,36 @@ exports.register = async (req, res) => {
         return res.status(409).json({ message: 'User with this login already exists' });
       };
 
-      const user = await User.create({ login, password: await bcrypt.hash(password, 10), avatar, phoneNumber });
+      const user = await User.create({ login, password: await bcrypt.hash(password, 10), avatar, phoneNumber});
       res.status(201).json({ message: 'User created ' + user.login });
     } else {
       res.status(400).json({ message: 'Bad request' });
     };
+  } catch(err) {
+    res.status(500).json({ message: err });
   }
-  catch(err) {
+};
+
+exports.login = async (req, res) => {
+  try {
+    const { login, password } = req.body;
+
+    if (login && typeof login === 'string' && password && typeof password === 'string') {
+      const user = await User.findOne({ login });
+
+      if (!user) {
+        res.status(400).json({ message: 'Login or password are incorrect' });
+      } else {
+        if (bcrypt.compareSync(password, user.password)) {
+          res.status(200).json({ message: 'Login successful '});
+        } else {
+          res.status(400).json({ message: 'Login or password are incorrect' });
+        };
+      };
+    } else {
+      res.status(400).send({ message: 'Bad request' });
+    };
+  } catch(err) {
     res.status(500).json({ message: err });
   }
 };
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -5,6 +5,7 @@ const auth = require('../controllers/auth.controller');
 const upload = require('../utils/imageUpload');
 
 router.post('/register', upload.single('avatar'), auth.register);
+router.post('/login', auth.login);
 
 module.exports = router;
 
