ee79eb44fa97a2e8658dd2d4f7161773343967ee
Sławomir Toporek <toporek.slawomir@gmail.com>
2024-06-21 23:59:33 +0200

    Add the EditAd component

--- client/src/components/pages/EditAd/EditAd.js
+++ client/src/components/pages/EditAd/EditAd.js
@@ -1,6 +1,56 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate, useParams } from 'react-router-dom';
+import { Row, Col } from 'react-bootstrap';
+import AdForm from '../../features/AdForm/AdForm';
+import { getUser } from '../../../redux/usersRedux';
+import { editAdRequest, fetchAds, getAdById } from '../../../redux/adsRedux';
+
 const EditAd = () => {
+
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const user = useSelector(getUser);
+  const dataAd = useSelector((state) => getAdById(state, id));
+
+  const handleSubmit = (ad) => {
+    const formData = new FormData();
+    formData.append("title", ad.title);
+    formData.append("content", ad.content);
+    formData.append("publicationDate", ad.publicationDate);
+    formData.append("price", ad.price);
+    if (ad.image) {
+      formData.append("image", ad.image);
+    };
+    formData.append("location", ad.location);
+
+    dispatch(editAdRequest(formData, id));
+    
+    navigate("/");
+  };
+
+  useEffect(() => {
+
+    // if (!user || dataAd.user._id !== user.id) {
+    //   navigate("/");
+    // };
+
+    if (dataAd && Object.keys(dataAd).length === 0) {
+      navigate("/");
+    };
+
+    if (!dataAd || (dataAd && Object.keys(dataAd).length === 0)) {
+      dispatch(fetchAds());
+    };
+  }, [dataAd, dispatch, navigate, user]);
   return(
-    <></>
+    <Row>
+        <Col className="col-12 col-sm-3 mx-auto">
+          <h1 className="my-3">Edit Ad</h1>
+          <AdForm action={handleSubmit} {...dataAd} />
+        </Col>
+    </Row>
   );
 };
 
--- client/src/redux/adsRedux.js
+++ client/src/redux/adsRedux.js
@@ -2,6 +2,10 @@ import { API_URL } from "../config";
 
 /* ACTIONS */
 export const getUser = (state) => state.user ? state.user.data : null;
+export const getAdById = ({ ads }, id) => 
+  ads && Array.isArray(ads.data)
+  ? ads.data.find((ad) => ad._id === id)
+  : null;
 
 // action name creator
 const reducerName = 'ads';
@@ -9,10 +13,12 @@ const createActionName = name => `app/${reducerName}/${name}`;
 
 export const LOAD_ADS = createActionName("LOAD_ADS");
 export const ADD_AD = createActionName("ADD_AD");
+export const EDIT_AD = createActionName("EDIT_AD");
 export const ERROR = createActionName("ERROR");
 
 export const loadAds = (payload) => ({ type: LOAD_ADS, payload });
 export const addAd = (payload) => ({ type: ADD_AD, payload });
+export const editAd = (payload) => ({ type: EDIT_AD, payload });
 export const setError = (payload) => ({ type: ERROR, payload });
 
 /* THUNKS */
@@ -59,6 +65,27 @@ export const addAdRequest = (ad) => async (dispatch) => {
   }
 };
 
+export const editAdRequest = (ad, id) => async (dispatch) => {
+
+  const options = {
+    method: 'PUT',
+    body: ad
+  };
+
+  try {
+    const response = await fetch(`${API_URL}/api/ads/${id}`, options);
+    if (response.ok) {
+      dispatch(fetchAds());
+    } else {
+      const errorData = await response.json();
+      dispatch(setError(errorData.message || 'Failed to update ad'));
+    }
+  } catch (e) {
+    dispatch(setError(e.message));
+  }
+};
+
+
 /* REDUCER */
 
 const initialState = {
@@ -70,6 +97,8 @@ const adsReducer = (statePart = initialState, action) => {
   switch (action.type) {
     case ADD_AD:
       return { ...statePart, data: [...statePart.data, action.payload], error: null };
+    case EDIT_AD:
+      return { ...statePart, data: statePart.data.map(ad => ad._id === action.payload._id ? action.payload : ad)};
     case LOAD_ADS:
       return { ...statePart, data: action.payload, error: null };
     case ERROR:
